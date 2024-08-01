5273dd2d04a7dcdfbf4f802efd7e47636a3b31d2
klam2k20 <klam2k17@gmail.com>
2024-08-01 10:43:10 -0400

    Create weather api request function

---
+++ src/api/weather.ts
@@ -0,0 +1,14 @@
+import axios from 'axios';
+import { TWeatherApiResponse } from '../lib/types';
+
+const API_KEY = import.meta.env.VITE_API_KEY;
+
+export default async function getWeather(lat: number, lon: number): Promise<TWeatherApiResponse[]> {
+  try {
+    const response = await axios.get<TWeatherApiResponse[]>(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=minutely,alerts&appid=${API_KEY}`);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching location data:', error);
+    throw error;
+  }
+}
