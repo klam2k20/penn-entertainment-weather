a0a507aa0fc3d2b3eab7717d62d7b65de19196fe
klam2k20 <klam2k17@gmail.com>
2024-08-01 16:24:45 -0400

    Add geolocation functionality

--- src/api/geocoding.ts
+++ src/api/geocoding.ts
@@ -3,7 +3,7 @@ import { TGeocodingApiResponse } from '../lib/types';
 
 const API_KEY = import.meta.env.VITE_API_KEY;
 
-export default async function getLocation(city: string, limit: number = 5): Promise<TGeocodingApiResponse[]> {
+async function getLocation(city: string, limit: number = 5): Promise<TGeocodingApiResponse[]> {
   try {
     const response = await axios.get<TGeocodingApiResponse[]>(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=${limit}&appid=${API_KEY}`);
     return response.data;
@@ -12,3 +12,17 @@ export default async function getLocation(city: string, limit: number = 5): Prom
     throw error;
   }
 }
+
+
+async function getCity(lat: number, lon: number): Promise<TGeocodingApiResponse[]> {
+  try {
+    const response = await axios.get<TGeocodingApiResponse[]>(`http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=1&appid=${API_KEY}`);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching location data:', error);
+    throw error;
+  }
+}
+
+export { getLocation, getCity }
+
