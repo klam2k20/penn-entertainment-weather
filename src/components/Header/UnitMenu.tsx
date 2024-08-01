be59b333bc3f7a01083e46d15e5f3e447fbf4b70
klam2k20 <klam2k17@gmail.com>
2024-08-01 00:35:07 -0400

    Create Dropdown component within UnitMenu

--- src/components/Header/UnitMenu.tsx
+++ src/components/Header/UnitMenu.tsx
@@ -7,6 +7,13 @@ export default function UnitMenu() {
     const [unit, setUnit] = useState<string>('imperial')
     const menuRef = useRef<HTMLDivElement>(null)
 
+    useEffect(() => {
+        document.addEventListener('mousedown', handleClickOutside)
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside)
+        }
+    }, [])
+
     const handleClickOutside = (event: MouseEvent | TouchEvent) => {
         if (
             menuRef.current &&
@@ -21,12 +28,30 @@ export default function UnitMenu() {
         setShowMenu(false)
     }
 
-    useEffect(() => {
-        document.addEventListener('mousedown', handleClickOutside)
-        return () => {
-            document.removeEventListener('mousedown', handleClickOutside)
-        }
-    }, [])
+    const Dropdown = () => {
+        return (
+            <div className="absolute right-2 top-10 flex w-fit flex-col items-end justify-center gap-1 rounded-md bg-zinc-700">
+                <button
+                    className={cn(
+                        'w-full rounded-md bg-transparent px-4 py-2 text-sm text-white transition-all',
+                        unit === 'imperial' && 'bg-white text-black'
+                    )}
+                    onClick={() => handlePress('imperial')}
+                >
+                    Imperial
+                </button>
+                <button
+                    className={cn(
+                        'w-full rounded-md bg-transparent px-4 py-2 text-sm text-white transition-all',
+                        unit === 'metric' && 'bg-white text-black'
+                    )}
+                    onClick={() => handlePress('metric')}
+                >
+                    Metric
+                </button>
+            </div>
+        )
+    }
 
     return (
         <div className="relative p-2 md:hidden" ref={menuRef}>
@@ -34,28 +59,7 @@ export default function UnitMenu() {
                 className="h-4 w-4 cursor-pointer"
                 onClick={() => setShowMenu((prev) => !prev)}
             />
-            {showMenu && (
-                <div className="absolute right-2 top-10 flex w-fit flex-col items-end justify-center gap-1 rounded-md bg-zinc-700">
-                    <button
-                        className={cn(
-                            'w-full rounded-md bg-transparent px-4 py-2 text-sm text-white transition-all',
-                            unit === 'imperial' && 'bg-white text-black'
-                        )}
-                        onClick={() => handlePress('imperial')}
-                    >
-                        Imperial
-                    </button>
-                    <button
-                        className={cn(
-                            'w-full rounded-md bg-transparent px-4 py-2 text-sm text-white transition-all',
-                            unit === 'metric' && 'bg-white text-black'
-                        )}
-                        onClick={() => handlePress('metric')}
-                    >
-                        Metric
-                    </button>
-                </div>
-            )}
+            {showMenu && <Dropdown />}
         </div>
     )
 }
