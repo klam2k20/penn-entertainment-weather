dd8677e2fbffef84f630d05152dcb488c6221ea9
klam2k20 <klam2k17@gmail.com>
2024-08-01 20:21:11 -0400

    Fix loading states

--- src/components/DailyStats/DailyStat.tsx
+++ src/components/DailyStats/DailyStat.tsx
@@ -1,5 +1,3 @@
-import { useWeather } from '../../contexts/WeatherContext'
-
 type Props = {
     title: string
     value: string
@@ -9,23 +7,10 @@ type Props = {
  * Displays a weather statistic with a title and value.
  */
 export default function DailyStat({ title, value }: Props) {
-    const { isLoading } = useWeather()
-
-    if (isLoading) return <DailyStatLoading />
-
     return (
         <div className="flex flex-col items-center rounded-md bg-zinc-700 p-8">
             <h2 className="font-semibold">{title}</h2>
             <p className="font-xl text-lg">{value}</p>
         </div>
-    )
-}
-
-/**
- * Displays a loading state for the DailyStat component.
- */
-function DailyStatLoading() {
-    return (
-        <div className="h-full w-full animate-pulse rounded-md bg-zinc-700"></div>
     )
 }
--- src/components/Forecast/Forecast.tsx
+++ src/components/Forecast/Forecast.tsx
@@ -1,5 +1,3 @@
-import { useWeather } from '../../contexts/WeatherContext'
-
 type Props = {
     date: string
     icon: string
@@ -10,24 +8,11 @@ type Props = {
  * Displays a single forecast item with the date, weather icon, and temperature.
  */
 export default function Forecast({ date, icon, temp }: Props) {
-    const { isLoading } = useWeather()
-
-    if (isLoading) return <ForecastLoading />
-
     return (
         <div className="flex min-h-36 min-w-16 flex-col items-center justify-center gap-2 rounded-md bg-zinc-700 p-2 lg:min-h-48 lg:min-w-32 lg:p-4">
             <span className="text-center">{date}</span>
             <img src={icon} className="h-16 w-16 lg:h-24 lg:w-24" />
             <span className="text-center text-xl font-semibold">{temp}°</span>
         </div>
-    )
-}
-
-/**
- * Displays a loading state for the Forecast component.
- */
-function ForecastLoading() {
-    return (
-        <div className="min-h-36 min-w-16 animate-pulse rounded-md bg-zinc-700 p-2 lg:min-h-48 lg:min-w-32 lg:p-4" />
     )
 }
