b6ff4116952bdd47c92d875f5201ddf23d957781
klam2k20 <klam2k17@gmail.com>
2024-08-01 20:58:39 -0400

    Styling

--- src/components/Forecast/Forecast.tsx
+++ src/components/Forecast/Forecast.tsx
@@ -9,7 +9,7 @@ type Props = {
  */
 export default function Forecast({ date, icon, temp }: Props) {
     return (
-        <div className="flex min-h-36 min-w-16 flex-col items-center justify-center gap-2 rounded-md bg-zinc-700 p-2 lg:min-h-48 lg:min-w-32 lg:p-4">
+        <div className="flex min-h-36 min-w-20 flex-col items-center justify-center gap-2 rounded-md bg-zinc-700 p-2 lg:min-h-48 lg:min-w-32 lg:p-4">
             <span className="text-center">{date}</span>
             <img src={icon} className="h-16 w-16 lg:h-24 lg:w-24" />
             <span className="text-center text-xl font-semibold">{temp}°</span>
